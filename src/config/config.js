a6d56321c3f345e2e1be3b26265e492ff0b4cd73
Vivek Singh Pundir <132738914+vivekdevp01@users.noreply.github.com>
2025-09-04 20:07:55 +0530

    Added again config

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER ,
-    password: process.env.DB_PASS ,
-    database: process.env.DB_NAME ,
-    host: process.env.DB_HOST,
-    dialect: "mysql"
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "mysql"
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "Abcd@1234",
+    database: process.env.DB_NAME || "schoolmanagement",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
-    dialect: "mysql"
-  }
+    dialect: process.env.DB_DIALECT || "mysql",
+  },
+
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "Abcd@1234",
+    database: process.env.DB_NAME || "schoolmanagement_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || "mysql",
+  },
+
+  production: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "XIFeCWGaBKYHqOBAeLFkxtwsZhyQIXHy",
+    database: process.env.DB_NAME || "railway",
+    host: process.env.DB_HOST || "maglev.proxy.rlwy.net",
+    port: process.env.DB_PORT || 23792,
+    dialect: process.env.DB_DIALECT || "mysql",
+  },
 };
-// mysql.railway.internal
-// root
-// GmktQwSRsszBKdYmrLOYDZfkbTYlHHAU
-// railway
-// 3306
